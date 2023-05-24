e4b448c77b0bad4a55d939477a711aaa96c88724
Drew Powers <1369770+drwpow@users.noreply.github.com>
2023-05-23 21:32:33 -0600

    Update matchers for vitest@0.31
    Listed as part of Vitest’s breaking changes with v0.31: https://github.com/vitest-dev/vitest/releases/tag/v0.31.0

--- src/extend-expect.ts
+++ src/extend-expect.ts
@@ -1,9 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import type { AxeMatchers } from "./matchers";
 
-declare global {
-  namespace Vi {
-    interface Assertion<T = any> extends AxeMatchers {}
-    interface AsymmetricMatchersContaining extends AxeMatchers {}
-  }
+declare module "vitest" {
+  interface Assertion<T = any> extends AxeMatchers {}
+  interface AsymmetricMatchersContaining extends AxeMatchers {}
 }
