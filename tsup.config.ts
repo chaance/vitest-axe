75b3254fd05df4938eb873ed9c142696c2f1b89b
koralle <mkhd7930@gmail.com>
2024-08-16 01:57:31 +0900

    chore: update `tsup.config.ts`

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,12 +2,10 @@ import { defineConfig } from "tsup"
 
 const entry = ["./src/index.ts", "./src/matchers.ts", "./src/extend-expect.ts"]
 
-export default defineConfig([
-  {
-    entry,
-    format: "esm",
-    sourcemap: true,
-    dts: true,
-    outDir: "dist",
-  },
-])
+export default defineConfig({
+  entry,
+  format: "esm",
+  sourcemap: true,
+  dts: true,
+  outDir: "dist",
+})
